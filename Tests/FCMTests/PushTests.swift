61a006a161a921b6e77f7c2619a95c849dccb00e
GErP83 <gurrka@gmail.com>
2024-04-11 11:35:37 +0200

    comment change

--- Tests/FCMTests/PushTests.swift
+++ Tests/FCMTests/PushTests.swift
@@ -49,19 +49,19 @@ final class PushTests: XCTestCase {
     }
 
     func testPush() async throws {
-        ///{
-        ///   "type": "service_account",
-        ///   "project_id": "your_project_id",
-        ///   "private_key_id": "your_private_key_id",
-        ///   "private_key": "your_private_key",
-        ///   "client_email": "your_client_email",
-        ///   "client_id": "your_client_id",
-        ///   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        ///   "token_uri": "https://oauth2.googleapis.com/token",
-        ///   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        ///   "client_x509_cert_url": "your_client_x509_cert_url",
-        ///   "universe_domain": "googleapis.com"
-        ///}
+        //{
+        //   "type": "service_account",
+        //   "project_id": "your_project_id",
+        //   "private_key_id": "your_private_key_id",
+        //   "private_key": "your_private_key",
+        //   "client_email": "your_client_email",
+        //   "client_id": "your_client_id",
+        //   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        //   "token_uri": "https://oauth2.googleapis.com/token",
+        //   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        //   "client_x509_cert_url": "your_client_x509_cert_url",
+        //   "universe_domain": "googleapis.com"
+        //}
 
         let credentials = FCMCredentials(
             type: type,
